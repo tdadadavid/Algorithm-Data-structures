b1f542bb500eb935d3424dc13fa188109d3f44c7
tdadadavid <davidtofunmidada@gmail.com>
2022-02-21 03:24:59 +0100

    wrote two algorithms for reversing a string

--- Algorithms/StringsAlgorithms/src/com/company/CountVowels.java
+++ Algorithms/StringsAlgorithms/src/com/company/CountVowels.java
@@ -1,14 +1,17 @@
 package com.company;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class CountVowels {
     public static void main(String[] args) {
-        String word = "hello";
-        System.out.println(countVowels(word));
+        String word = "   E      nkll"; // returns 1
+        System.out.println(countVowel(word));
     }
 
     private static int countVowels(String word){
+        if(word == null)
+            return 0;
 
         word = word.toLowerCase();
 
@@ -23,6 +26,26 @@ public class CountVowels {
 
         for (char character : word.toCharArray())
             if(vowels.containsKey(character))
+                count++;
+
+        return count;
+    }
+
+    private static int countVowel(String sentence){
+        if (sentence == null)
+            return 0;
+
+        int count = 0;
+
+        HashSet<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        for (char character : sentence.toLowerCase().toCharArray())
+            if (vowels.contains(character))
                 count++;
 
         return count;
--- Algorithms/StringsAlgorithms/src/com/company/ReverseString.java
+++ Algorithms/StringsAlgorithms/src/com/company/ReverseString.java
@@ -1,4 +1,51 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class ReverseString {
+    public static void main(String[] args) {
+        String word = "";
+        System.out.println(reverseStrings(word));
+    }
+
+    // very bad approach
+    private static String reverseString(String string){
+        if (string == null)
+            return "";
+
+        // convert string to array
+        char[] stringArray = string.toCharArray(); // O(n)
+        int j = stringArray.length-1;
+
+        // reverse the array then
+        for(int i = 0; i < stringArray.length / 2; i++){  // O(logn)
+            char temp = stringArray[j];
+            stringArray[j] = stringArray[i];
+            stringArray[i] = temp;
+            --j;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char character : stringArray) // O(n)
+            stringBuilder.append(character); // O(1)
+
+
+        // convert the array back to string
+        return String.valueOf(stringBuilder); // total time = O(n) + O(log n) + O(n)
+    }
+
+    // best approach
+    private static String reverseStrings(String string)
+    {
+        if (string == null || string.equals(""))
+            return "";
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = string.length() -1; i >= 0; i--) // O(n)
+            stringBuilder.append(string.charAt(i)); // O(1)
+
+        return stringBuilder.toString(); // total= O(n) + O(1)
+    }
 }
