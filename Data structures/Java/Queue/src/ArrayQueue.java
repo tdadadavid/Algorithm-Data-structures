3f065121c32f573e8de3064c4f69774d0715f2ae
tdadadavid <davidtofunmidada@gmail.com>
2022-05-27 23:43:35 +0100

    Built a queue using a static array

---
+++ Data structures/Java/Queue/src/ArrayQueue.java
@@ -0,0 +1,86 @@
+
+// A queue can be implemented using
+//  - An array
+//  - A linked-list
+//  - A stack
+
+// Here I'm using an array to make a queue
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class ArrayQueue<E> {
+
+    private int front = 0;
+    private int rear = 0;
+
+    private E[] element;
+
+    public ArrayQueue(int size){
+        this.element =  (E[]) new Object[size];
+    }
+    public ArrayQueue(){
+        this(10);
+    }
+
+    // [10,20,30,0]
+
+    // enqueue
+    public void enqueue(E value){
+        if (isFull()) throw new IllegalStateException("Queue is already full");
+
+        this.element[rear++] = value;
+    }
+
+    // dequeue
+    public E dequeue(){
+        if (isEmpty()) throw new IllegalStateException("Queue is empty");
+
+        // remove the first element from the queue
+        E elementAtFrontOfQueue = this.element[front];
+
+        // move the pointer forward to simulate
+        // the removal of the first element
+        front++;
+
+        return elementAtFrontOfQueue;
+    }
+
+    // poll
+    public E poll(){
+        if(isEmpty()) return null;
+
+        // remove the first element from the queue
+        E elementAtFrontOfQueue = this.element[front];
+
+        // move the pointer forward to simulate
+        // the removal of the first element
+        front++;
+
+        return elementAtFrontOfQueue;
+    }
+
+    // peek
+    public E peek(){
+        if (isEmpty()) return null;
+
+        E elementAtFrontOfQueue = this.element[front];
+
+        return elementAtFrontOfQueue;
+    }
+
+    // isEmpty
+    public boolean isEmpty(){
+        return front == rear;
+    }
+
+    // isFull
+    public boolean isFull(){
+        return rear > this.element.length-1;
+    }
+
+    public String toString(){
+        return Arrays.toString(this.element);
+    }
+
+}
--- Data structures/Java/Queue/src/MyQueue.java
+++ Data structures/Java/Queue/src/MyQueue.java
@@ -1,19 +1,106 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+
+/**
+ *  A Queue is a fifo data structure, meaning the first element
+ *  that gets into the queue gets out first (first in first out)
+ *
+ *  OPERATION IN A QUEUE
+ *
+ *  1. Enqueue: Adds an element to the front of the queue.
+ *      Time complexity: Constant time O(1).
+ *
+ *  2. Dequeue: Removes and returns an element from the front of the queue.
+ *              Throws error if there is no element in the queue.
+ *      Time complexity: Constant time O(1)
+ *
+ * 3. Peek: Same as Dequeue (returns null if there is no element in the queue).
+ *
+ * 4. Poll: Returns first element in the queue (returns null queue is empty)
+ *          Doesn't remove the element *
+ *     Time complexity: Constant time O(1)
+ *
+ *  Classes that implement Queues in Java
+ *      - linked-list
+ *      - ArrayDequeue (Array double ended queue)
+ *      - And many more
+ *
+ *
+ *  Real Life use cases for a queue includes
+ *  1. Web servers: Queues are used to handle request and task
+ *                  sent to web servers example of real life application
+ *                  is RabbitMQ
+ *
+ *  2. Operating System:  Queues alongside with stacks are used to manage processes
+ *                        in the system
+ *
+ *  3. Printers: Have you ever printed multiple documents or files before? how were
+ *               they printed in the order you sent them? Here is the power of queue😎😎
+ *               The printer queue stores the order in which they come and attends to each
+ *               process in that order, then when the file has been printer it moves on to
+ *               the next file sent (dequeue)
+ *
+**/
 
 public class MyQueue {
     public static void main(String[] args){
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
 
-        reverse2(queue);
-        System.out.println(queue);
+        ArrayQueue<Integer> numbers = new ArrayQueue<>(7);
+        numbers.enqueue(20);
+        numbers.enqueue(10);
+        numbers.enqueue(30);
+        numbers.enqueue(19);
+        numbers.enqueue(39);
+        numbers.enqueue(129);
+        numbers.enqueue(309);
+
+        // should remove the two first values from the queue
+        int firstValue = numbers.dequeue();
+        int secondValue = numbers.dequeue();
+        System.out.println("First value(dequeue): " + firstValue);
+        System.out.println("First value(dequeue): " + secondValue);
+
+        // it should return the same number
+        // since it doesn't remove the first element
+        // in the queue
+
+        int firstElement = numbers.peek();
+        int stillFirstElement = numbers.peek();
+        int stillTheSameFirstElement = numbers.peek();
+        System.out.println("First element(peek): " + firstElement);
+        System.out.println("First element(peek): " + stillFirstElement);
+        System.out.println("First element(peek): " + stillTheSameFirstElement);
+
+
+        // should remove the element at the front
+        // of the queue
+
+        int firstInQueue = numbers.poll();
+        System.out.println("First element(poll): " + firstInQueue);
+        int newFirstInQueue = numbers.poll();
+        System.out.println("First element(poll): " + newFirstInQueue);
+        numbers.poll();
+        numbers.poll();
+        int lastElement = numbers.poll();
+        System.out.println("First element(poll): " + lastElement);
+
+        // return null since no element is in the queue
+        System.out.println(numbers.poll());
+        System.out.println(numbers.poll());
+        System.out.println(numbers.poll());
+
+        // should return null since no element is in the queue
+        System.out.println(numbers.peek());
+
+        // should throw illegalStateException since
+        // there is no element in the queue
+
+        System.out.println(numbers.dequeue());
+
+        System.out.println(numbers);
+
     }
 
+    // Using an array
     public static void reverse(Queue<Integer> queue){
         int arraySize = queue.size();
         int[] storageArr = new int[arraySize];
@@ -27,6 +114,7 @@ public class MyQueue {
         }
     }
 
+    // Using an array list
     public static void reverse2(Queue<Integer> queue){
         List<Integer> storageList = new ArrayList<>();
 
@@ -37,5 +125,18 @@ public class MyQueue {
         int sizeOfList = storageList.size();
         while (!storageList.isEmpty())
             queue.add(storageList.remove(--sizeOfList));
+    }
+
+    //  Using a stack
+    public static void reverse3(Queue<Integer> queue){
+        Stack<Integer> storage = new Stack<>();
+
+        while(!queue.isEmpty()){
+            storage.push(queue.remove());
+        }
+
+        while(!storage.isEmpty()){
+            queue.add(storage.pop());
+        }
     }
 }
