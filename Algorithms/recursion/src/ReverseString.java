6f8e26a8c7c7e535f9e48187bddf7c9c9c73fd31
tdadadavid <davidtofunmidada@gmail.com>
2022-06-09 21:16:09 +0100

    reversed a string

---
+++ Algorithms/recursion/src/ReverseString.java
@@ -0,0 +1,25 @@
+public class ReverseString {
+
+    public static void main(String[] args){
+        String word = "backward";
+        String testcase1 = "a";
+        String testcase2 = "ab";
+        String testcase3 = "";
+
+        String result  = reverseString(word);
+        String result1 = reverseString(testcase1);
+        String result2 = reverseString(testcase2);
+        String result3 = reverseString(testcase3);
+
+        System.out.println(word + " => " + result);
+        System.out.println(testcase1 + " => " + result1);
+        System.out.println(testcase2 + " => " + result2);
+        System.out.println(testcase3 + " => " + result3);
+    }
+
+    public static String reverseString(String input){
+        if (input.equals("")) return "";
+
+        return reverseString(input.substring(1)) + input.charAt(0);
+    }
+}
